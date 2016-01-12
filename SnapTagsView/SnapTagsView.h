855fb21d7682794924a22fadc11c78dcb9e4fa5d
Niklas Saers <niklas@saers.com>
2016-01-12 15:04:03 +0100

    Initial Commit

---
+++ SnapTagsView/SnapTagsView.h
@@ -0,0 +1,19 @@
+//
+//  SnapTagsView.h
+//  SnapTagsView
+//
+//  Created by Niklas Saers on 12/01/16.
+//  Copyright © 2016 Snapsale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnapTagsView.
+FOUNDATION_EXPORT double SnapTagsViewVersionNumber;
+
+//! Project version string for SnapTagsView.
+FOUNDATION_EXPORT const unsigned char SnapTagsViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapTagsView/PublicHeader.h>
+
+
